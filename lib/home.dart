db08c449f85cfd0f7a6daf6fb46df0a0795114cc
nararya <87893990+nararyacodetechy@users.noreply.github.com>
2022-05-19 00:01:26 +0800

    Create Project

---
+++ lib/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ListViews',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Test')),
+        // body: BodyLayout(),
+      ),
+    );
+  }
+}
